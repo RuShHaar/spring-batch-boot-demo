d66df805427a83d342ca22530d8975358f53b79d
Sebastian Siegling <s.sgl@t-online.de>
2021-06-30 09:35:02 +0200

    List implement

--- src/main/java/de/moscon/etl/steps/nr01_customerData/CustomerProcessor.java
+++ src/main/java/de/moscon/etl/steps/nr01_customerData/CustomerProcessor.java
@@ -8,7 +8,12 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
@@ -23,14 +28,31 @@ public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
 	public Customer process(Customer item) throws Exception {
 		shopSimulator.readProductPos(0);
 		saleList = shopSimulator.CACHE;
+		SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+		Date firstBuy = null;
+//		List<Sale> saleListCutomer= (List<Sale>) saleList.get(item.getId());
+		try {
+			firstBuy = sdformat.parse("2099-12-30");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		for (Sale sale : saleList) {
+			if (sale.getId()== item.getId() && sale.getTime().before(firstBuy)) {
+				firstBuy = sale.getTime();
+			}
+		}
+		Date registrationDate = item.getRegistrationDate();
+		if (registrationDate.after(firstBuy)) {
+                item.setRegistrationDate(firstBuy);
+            }
 
-		System.out.println(saleList.get(0));
+//		System.out.println(saleList.get(0));
 
 
 		item.setPseudonym("customer_"+item.getFirstname().toLowerCase());
 		item.setFirstname(null);
 
-		if(item.getRegistrationDate() < )
+//		if(item.getRegistrationDate() < saleList.get(1).getTime())
 
 		return item;
 	}
